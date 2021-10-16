02bb14df337eec179a3fd07ccb2e8275e258a143
b72u68 <mdinh@hawk.iit.edu>
2021-10-16 04:53:58 -0500

    Refactor

--- src/background.js
+++ src/background.js
@@ -1,13 +1,6 @@
-let easy = true;
-let medium = true;
-let hard = true;
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ easy });
-  chrome.storage.sync.set({ medium });
-  chrome.storage.sync.set({ hard });
+  chrome.storage.sync.set({ easy: true });
+  chrome.storage.sync.set({ medium: true });
+  chrome.storage.sync.set({ hard: true });
   chrome.storage.sync.set({ theme: "light" });
-  console.log(
-    `The default filter is set to easy=${easy}, medium=${medium}, hard=${hard}`
-  );
 });
