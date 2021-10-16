6aa432cbbe6d77d3164d880e460afd321fab8299
b72u68 <mdinh@hawk.iit.edu>
2021-10-16 03:37:47 -0500

    Added dark/light theme

--- src/background.js
+++ src/background.js
@@ -6,6 +6,7 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ easy });
   chrome.storage.sync.set({ medium });
   chrome.storage.sync.set({ hard });
+  chrome.storage.sync.set({ theme: "light" });
   console.log(
     `The default filter is set to easy=${easy}, medium=${medium}, hard=${hard}`
   );
--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,20 @@
+let title = document.getElementById("title");
+
 let applyBtn = document.getElementById("applyBtn");
 let clearBtn = document.getElementById("clearBtn");
+let themeBtn = document.getElementById("themeBtn");
 
 let easyOption = document.getElementById("easyOption");
 let mediumOption = document.getElementById("mediumOption");
 let hardOption = document.getElementById("hardOption");
+
+let easyOverlay = document.getElementById("easyOverlay");
+let mediumOverlay = document.getElementById("mediumOverlay");
+let hardOverlay = document.getElementById("hardOverlay");
+
+chrome.storage.sync.get("theme", ({ theme }) => {
+  setToTheme(theme);
+});
 
 chrome.storage.sync.get("easy", ({ easy }) => {
   easyOption.checked = easy;
@@ -17,16 +28,21 @@ chrome.storage.sync.get("hard", ({ hard }) => {
   hardOption.checked = hard;
 });
 
+themeBtn.addEventListener("click", async () => {
+  if (themeBtn.className === "lightBtn") {
+    chrome.storage.sync.set({ theme: "light" });
+  } else {
+    chrome.storage.sync.set({ theme: "dark" });
+  }
+  setToTheme();
+});
+
 applyBtn.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  if (!easyOption.checked && !mediumOption.checked && !hardOption.checked) {
-    clearCheckboxAndStorage();
-  } else {
-    chrome.storage.sync.set({ easy: easyOption.checked });
-    chrome.storage.sync.set({ medium: mediumOption.checked });
-    chrome.storage.sync.set({ hard: hardOption.checked });
-  }
+  chrome.storage.sync.set({ easy: easyOption.checked });
+  chrome.storage.sync.set({ medium: mediumOption.checked });
+  chrome.storage.sync.set({ hard: hardOption.checked });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
@@ -43,6 +59,18 @@ clearBtn.addEventListener("click", async () => {
     target: { tabId: tab.id },
     function: clearFilters,
   });
+});
+
+easyOverlay.addEventListener("click", () => {
+  easyOption.checked = !easyOption.checked;
+});
+
+mediumOverlay.addEventListener("click", () => {
+  mediumOption.checked = !mediumOption.checked;
+});
+
+hardOverlay.addEventListener("click", () => {
+  hardOption.checked = !hardOption.checked;
 });
 
 function applyFilters() {
@@ -93,4 +121,25 @@ function clearCheckboxAndStorage() {
   easyOption.checked = true;
   mediumOption.checked = true;
   hardOption.checked = true;
+}
+
+function setToTheme(theme) {
+  chrome.storage.sync.get("theme", ({ theme }) => {
+    switch (theme) {
+      case "light":
+        themeBtn.className = "darkBtn";
+        themeBtn.innerText = "Dark";
+        title.style.color = "black";
+        document.body.style.backgroundColor = "white";
+        break;
+      case "dark":
+        themeBtn.className = "lightBtn";
+        themeBtn.innerText = "Light";
+        title.style.color = "white";
+        document.body.style.backgroundColor = "#282b2d";
+        break;
+      default:
+        setTheme("light");
+    }
+  });
 }
