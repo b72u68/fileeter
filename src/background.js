c964bea16ca50a7cc4d6f1c4c2efb2c7288727be
b72u68 <mdinh@hawk.iit.edu>
2021-10-17 00:40:01 -0500

    Fixed extension active in restricted tab

--- src/background.js
+++ src/background.js
@@ -1,23 +1,23 @@
 chrome.runtime.onInstalled.addListener(async () => {
-  chrome.tabs.onActivated.addListener(async (activeInfo) => {
-    const tab = await chrome.tabs.get(activeInfo.tabId);
+  chrome.storage.sync.set({ easy: true });
+  chrome.storage.sync.set({ medium: true });
+  chrome.storage.sync.set({ hard: true });
 
-    const urlPattern = /(https|http):\/\/leetcode.com\/company\/.*/g;
-
-    if (tab.url && tab.url.match(urlPattern)) {
-      chrome.storage.sync.set({ easy: true });
-      chrome.storage.sync.set({ medium: true });
-      chrome.storage.sync.set({ hard: true });
-
-      chrome.storage.sync.get("theme", ({ theme }) => {
-        if (!theme) {
-          chrome.storage.sync.set({ theme: "light" });
-        }
-      });
-
-      chrome.action.enable(tab.id);
-    } else {
-      chrome.action.disable(tab.id);
+  chrome.storage.sync.get("theme", ({ theme }) => {
+    if (!theme) {
+      chrome.storage.sync.set({ theme: "light" });
     }
   });
+});
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  const tab = await chrome.tabs.get(activeInfo.tabId);
+
+  const urlPattern = /(https|http):\/\/leetcode.com\/company\/.*/g;
+
+  if (tab.url && tab.url.match(urlPattern)) {
+    chrome.action.enable(tab.id);
+  } else {
+    chrome.action.disable(tab.id);
+  }
 });
